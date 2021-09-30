09f4ee3c388b3b74106f899b404f7f5ae1cc8045
rashi-ash <rashidav27@gmail.com>
2021-09-30 22:51:29 +0530

    error fixed

--- lib/card.dart
+++ lib/card.dart
@@ -10,34 +10,32 @@ class contain extends StatelessWidget {
       height: MediaQuery.of(context).size.height / 5.5,
       width: MediaQuery.of(context).size.width / 2.4,
       decoration: BoxDecoration(
-        boxShadow: [
+        boxShadow: const [
           BoxShadow(
             color: Color(0XFFf0f0f0),
             offset: Offset(0.0, 1.0),
             blurRadius: 6.0,
           ),
         ],
-        color: Color(0XFFffffff),
+        color:const Color(0XFFffffff),
         borderRadius: BorderRadius.circular(20),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50),
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Color(0XFFd7dee9),
             child: CircleAvatar(
-              radius: 30,
-              backgroundColor: Color(0XFFd7dee9),
-              child: CircleAvatar(
-                backgroundImage: AssetImage(path1),
-              ),
+              backgroundImage: AssetImage(path1),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 15,
           ),
           Text(
             path2,
-            style: TextStyle(
+            style:const TextStyle(
               fontWeight: FontWeight.normal,
               color: Colors.black54,
               fontSize: 15,
